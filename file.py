013cfb7dcf55e49d94fbea8059afc4ed14ec78cb
RonyGigi <ronygigi@gmail.com>
2017-07-31 11:43:33 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,11 +1,12 @@
 import webbrowser
 url=""
 n=0
-f=open("D:\Github\link.txt","r")
+location=input("Enter the path of file in which links are stored")
+f=open(location,"r")
 while f.readline()!='':
    n+=1
 f.close()
-f=open("D:\Github\link.txt","r")
+f=open(location,"r")
 firefox_path="C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"
 webbrowser.register('firefox',None,webbrowser.BackgroundBrowser(firefox_path),1)
 line=0
